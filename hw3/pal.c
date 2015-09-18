c13075c078cb2cfecb04390a5b2ecb96382378ff
Jason Boutte <jboutte@mail.csuchico.edu>
2015-09-17 22:06:39 -0700

    Adds palindrone counting and comments

--- hw3/pal.c
+++ hw3/pal.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ * Uncomment to print debug information. See following sample:
+ *
+ * Input: aaba
+ *
+ * i1 = 0 i2 = 0 a    the start/end index of the input and the palidrone
+ * i1 = 0 i2 = 1 aa
+ * aaba               subset we considered
+ * 1100               result of the subset
+ * i1 = 1 i2 = 1 a
+ * i1 = 1 i2 = 3 aba
+ *  aba
+ *  101
+ * i1 = 2 i2 = 2 b
+ *   ba
+ *   10
+ * i1 = 3 i2 = 3 a
+ *    a
+ *    1
+ */ 
+//#define DEBUG
+
+// Only need to change if the input file has more than 1024 characters
 #define MAX_INPUT 1024
 
 int main(int argc, char **argv) {
   FILE *fp;
   int size;
   int x, y, z;
+  int pal_count;
   char idata[MAX_INPUT];
   int matrix[MAX_INPUT][MAX_INPUT];
 
@@ -20,6 +44,7 @@ int main(int argc, char **argv) {
   
   size = 0;
 
+  // Read in file until we get EOF or a newline
   while (!feof(fp)) {
     idata[size] = getc(fp);
 
@@ -32,20 +57,19 @@ int main(int argc, char **argv) {
 
   fclose(fp); 
 
-  fprintf(stdout, "Read %d characters\n", size);
-
+  // Initialize array to zeros, except for the principal diagonal and its
+  // preceeding diagnol
   for (x = 0; x < size; ++x) {
-    fprintf(stdout, "%c\t", idata[x]);
-
-    matrix[x][x] = 1;
-
-    if (x > 0) {
-      matrix[x][x-1] = 1;
+    for (y = 0; y < size; ++y) {
+      if (x == y || x == y+1) {
+        matrix[x][y] = 1;
+      } else {
+        matrix[x][y] = 0;
+      }
     }
   }
 
-  fprintf(stdout, "\n");
-
+  // Uses dp to find all substrings containing a palindrone
   for (x = 1; x < size; ++x) {
     for (y = x; y < size; ++y) {
       if (matrix[y-x+1][y-1] == 1) {
@@ -55,15 +79,49 @@ int main(int argc, char **argv) {
       }
     }
   }
-  
-  fprintf(stdout, "\n");
 
+  pal_count = 0;
+
+  // Count how many palindrons were found.
   for (x = 0; x < size; ++x) {
-    for (y = 0; y < size; ++y) {
-      fprintf(stdout, "%d\t", matrix[x][y]);
+    for (y = x; y < size; ++y) {
+      if (matrix[x][y]  == 1) {
+        pal_count++;
+      } 
     }
+  }
+
+  fprintf(stdout, "Found %d palidrones\n", pal_count);
+
+#ifdef DEBUG
+  for (x = 0; x < size; ++x) {
+    for (y = x; y < size; ++y) {
+      if (matrix[x][y] == 1) {
+        fprintf(stdout, "i1 = %d i2 = %d ", x, y);
+
+        for (z = x; z <= y; ++z) fprintf(stdout, "%c", idata[z]);
+
+        fprintf(stdout, "\n");
+      }
+    }
+
+    for (y = 0; y < x; ++y) fprintf(stdout, " ");
+
+    for (y = x; y < size; ++y) {
+      fprintf(stdout, "%c", idata[y]);
+    }
+
+    fprintf(stdout, "\n");
+
+    for (y = 0; y < x; ++y) fprintf(stdout, " ");
+
+    for (y = x; y < size; ++y) {
+      fprintf(stdout, "%d", matrix[x][y]);
+    }
+
     fprintf(stdout, "\n");
   }
+#endif
 
   return 0;
 }
